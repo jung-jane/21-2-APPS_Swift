77cc8fd5a563147791a7ad82db3d216330cdbc67
Jung Jein <o0o1910419@sookmyung.ac.kr>
2021-09-15 16:30:02 +0900

    210909-15
    swift study first week
    - 변수, 상수 선언 및 스위프트 기본 규칙

---
+++ week01/MyPlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+var account = 100;
+
+//bought fuel for car
+account = account - 30;
+
+//got some snacks
+account = account - 5;
+
+// got toothpaste
+account = account - 5;
+
+
+/*
+ 여러줄 주석은
+ 이렇게 작성하는 겁니다
+ */
+
+print(45);
+print("Hello World");
+print(account);
+
