5ae4a9caf093a0e45f72a23e4d04c6bf7b960801
Jung Jein <o0o1910419@sookmyung.ac.kr>
2021-11-09 14:37:11 +0900

    211006-27 Section9 : BlogApp
    근데... 텍스트 view가 사라짐.. where..

---
+++ week04/MyDailyBlog/MyDailyBlog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyDailyBlog
+//
+//  Created by 정제인 on 2021/11/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
